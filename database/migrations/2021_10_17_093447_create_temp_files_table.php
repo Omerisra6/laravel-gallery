f4072845219e216ad8e2dd01594b5869479c152c
Omerisra06 <Omerisraeli6@gmail.com>
2023-05-23 19:27:21 +0300

    Fix: Migrations names fixed

--- database/migrations/2021_10_17_093447_create_temp_files_table.php
+++ database/migrations/2021_10_17_093447_create_temp_files_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class TempFileTable extends Migration
+class CreateTempFilesTable extends Migration
 {
     /**
      * Run the migrations.
