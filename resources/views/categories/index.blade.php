02d269c69b0c99e9b7b33b77b5302896791a723b
Omer Israeli <94297319+Omerisra6@users.noreply.github.com>
2025-04-19 15:51:51 +0300

    Add resources/views/categories/index.blade.php

---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Categories') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($categories as $category)
+                                <tr>
+                                    <th scope="row">{{ $category->id }}</th>
+                                    <td>{{ $category->name }}</td>
+                                    <td>
+                                        <a href="{{ route('categories.edit', $category) }}" class="btn btn-primary btn-sm">Edit</a>
+                                        <form action="{{ route('categories.destroy', $category) }}" method="POST" style="display: inline-block;">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you want to delete this category?')">Delete</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+
+                    <a href="{{ route('categories.create') }}" class="btn btn-success">Create New Category</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
